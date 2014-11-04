185eb7645a108070012f7f072738aafeeae990f1
unknown <n_ohno@ent-pc159.entermotion.local>
2014-11-04 17:04:28 +0900

    demo version

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@
 
 var caloryn = angular.module('caloryn', ['ngRoute','angularLocalStorage']);
 
+caloryn.controller('wrapperCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $scope.searchText = '';
+    $scope.checkDataArea = function () {
+        $rootScope.$broadcast('filterUpdate', $scope.searchText);
+    }
+    $scope.$on('addItem', function (e, item) {
+        $scope.searchText = '';
+        $scope.checkDataArea();
+    });
+}]);
+
 caloryn.controller('MessageCtrl', ['$scope', function($scope) {
     $scope.message = '';
     $scope.$on('errorMessage', function (e, data) {
@@ -9,14 +20,37 @@ caloryn.controller('MessageCtrl', ['$scope', function($scope) {
     });
 }]);
 
-
-caloryn.controller('SearchCtrl', ['$scope', function ($scope) {
-    $scope.search = function (value) {
-        return "searching "+value;
+caloryn.controller('itemsCtrl', ['$scope', function ($scope) {
+    $scope.items = [];
+    $scope.totalResult = 0;
+    $scope.remove = function (index) {
+        $scope.items.splice(index, 1);
+        $scope.resetTotalResult();
     }
+    $scope.calc = function (index) {
+        var data = $scope.items[index];
+        data.result = Math.round(data.calory * data.gram);
+        $scope.resetTotalResult();
+    }
+    $scope.resetTotalResult = function () {
+        var x, len;
+        $scope.totalResult = 0;
+        for (x = 0, len = $scope.items.length; x < len; x++) {
+            $scope.totalResult += $scope.items[x].result;
+        }
+    }
+    $scope.$on('addItem', function (e, data) {
+        $scope.items.push({
+            "name": data.name,
+            "gram": 0,
+            "calory": data.calory,
+            "result": 0
+        });
+    });
 }]);
 
 caloryn.controller('dataCtrl', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
+    $scope.searchText = $scope.$parent.searchText;
     $scope.data = [];
     $http({
         method: 'get',
@@ -30,7 +64,10 @@ caloryn.controller('dataCtrl', ['$scope', '$rootScope', '$http', function ($scop
         $rootScope.$broadcast('errorMessage', '通信中にエラーが発生しました')
     });
     $scope.click = function (data) {
-        console.log(data);
+        $rootScope.$broadcast('addItem', data);
     }
+    $scope.$on('filterUpdate', function (e, text) {
+        $scope.searchText = text;
+    });
 }]);
 
